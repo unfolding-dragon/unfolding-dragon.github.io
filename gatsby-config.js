4d15b2dd4a42c0ec28578c040fe39c178f38d6bd
Tiago <tsomda@gmail.com>
2019-08-11 11:30:22 -0700

    updated configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "Unfolding Dragon",
+    author: "Tiago",
+    description: "Simple Rules can yield Beautiful Patterns"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
